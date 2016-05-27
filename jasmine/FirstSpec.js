3935242b529aa37b140a9efb939af1909e4c0749
Nerielle <nelya.iva@gmail.com>
2016-05-27 10:33:20 +0300

    +custom matcher

--- jasmine/FirstSpec.js
+++ jasmine/FirstSpec.js
@@ -1,5 +1,23 @@
 describe('FirstTestsClass', function () {
+    beforeEach(function () {
+        jasmine.addMatchers({
+            toBeSeven: function () {
+                return {
+                    compare: function (actual, expected) {
+                        return {
+                            pass: actual === 7
+                        };
+                    }
+                };
+            }
+        });
+    });
+
     it('should be true', function () {
         expect(true).toBeTruthy();
     });
+    it('should be seven', function () {
+        expect(7).toBeSeven();
+    });
+
 });

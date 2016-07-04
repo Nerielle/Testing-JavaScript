88145226c83803c68a0e2b72e2dc6ffc42bc46f0
Nerielle <nelya.iva@gmail.com>
2016-07-04 14:58:23 +0300

    spyOn

--- jasmine/SpiesSpec.js
+++ jasmine/SpiesSpec.js
@@ -4,17 +4,36 @@ var Sut =
         this.callingCallback = function (cb) {
             cb();
         };
+        this.getNumber = function () {
+            return 5;
+        };
+        this.doSomething = function () {
+            console.log('doSomething');
+        };
     };
 
 describe('Spies', function () {
-    var sut, spy = null;
+    var sut;
     beforeEach(function () {
         sut = new Sut();
 
     });
     it('spy on callback', function () {
-        spy = jasmine.createSpy('spy');
+        var spy = jasmine.createSpy('spy');
         sut.callingCallback(spy);
         expect(spy).toHaveBeenCalled();
+    });
+    it('spy on method of existing object', function () {
+        var spy = spyOn(sut, 'doSomething');
+
+        sut.doSomething();
+
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('mocking return value', function () {
+        var spy = spyOn(sut, 'getNumber').and.returnValue(10);
+
+        expect(sut.getNumber()).toEqual(10);
     });
 });
